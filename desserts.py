cdb52e27e9b7bd0690f4733b414e1e44ba808c0a
Jackie Kinsler <jackiekinsler@gmail.com>
2020-05-11 12:16:55 -0700

    Modified Cupcake class to include __init__ and add_stock methods.

--- desserts.py
+++ desserts.py
@@ -3,12 +3,23 @@
 
 class Cupcake:
     """A cupcake."""
+    cache = {}
+
+    def __init__(self, name, flavor, price, qty = 0):
+      self.name = name
+      self.flavor = flavor
+      self.price = price
+      self.qty = qty
+
+      self.cache[self.name] = self
 
     def __repr__(self):
         """Human-readable printout for debugging."""
 
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
 
+    def add_stock(self, amount):
+      self.qty += amount
 
 if __name__ == '__main__':
     import doctest
