9fe7843b737ab06a5bbf777b9e89162363bde935
Jackie Kinsler <jackiekinsler@gmail.com>
2020-05-11 12:23:05 -0700

    Added add_stock method to Cupcake class

--- desserts.py
+++ desserts.py
@@ -6,6 +6,7 @@ class Cupcake:
     cache = {}
 
     def __init__(self, name, flavor, price, qty = 0):
+      
       self.name = name
       self.flavor = flavor
       self.price = price
@@ -19,7 +20,18 @@ class Cupcake:
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
 
     def add_stock(self, amount):
+      
       self.qty += amount
+
+    def sell(self, amount):
+      
+      if self.qty == 0:
+        print('Sorry, these cupcakes are sold out')
+      elif amount >= self.qty:
+        self.qty = 0
+      else:
+        self.qty -= amount
+
 
 if __name__ == '__main__':
     import doctest
@@ -32,3 +44,12 @@ if __name__ == '__main__':
     doctest.master.summarize(1)
     if result.failed == 0:
         print('ALL TESTS PASSED')
+
+c = Cupcake('Strawberry Fields', 'strawberry', 5.0)
+print(c.name)
+print(c.flavor)
+print(c.qty)
+
+print(Cupcake.cache)
+c.add_stock(10)
+print(Cupcake.cache)
